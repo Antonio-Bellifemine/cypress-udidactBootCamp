4b4b09ed5ce4a56652c4a0adfc29fc1fff354cfa
abellifemine <abellifemine@bluegracegroup.com>
2020-10-04 00:23:22 -0400

    make it work

--- cypress/integration/tests/basicTest/test.registerPage.spec.js
+++ cypress/integration/tests/basicTest/test.registerPage.spec.js
@@ -5,7 +5,7 @@ describe('test conduit webapp', () => {
         cy.viewport(1920, 1080);
         cy.visit('register');
         cy.get('a:contains("Sign in")').click()
-        cy.get('a:contains("Have an account?")').click()
+        cy.get('a:contains("Need an account?")').click()
         cy.get('input:eq(0)').should('be.visible')
         cy.get('input:eq(1)').should('be.visible')
         cy.get('input:eq(2)').should('be.visible')
@@ -15,7 +15,7 @@ describe('test conduit webapp', () => {
         cy.viewport(1920, 1080);
         cy.visit('register');
         cy.get('a:contains("Sign in")').click()
-        cy.get('a:contains("Have an account?")').click()
+        cy.get('a:contains("Need an account?")').click()
         cy.get('input[placeholder="Username"]').should('be.visible')
         cy.get('input[placeholder="Email"]').should('be.visible')
         cy.get('input[type="password"]').should('be.visible')
