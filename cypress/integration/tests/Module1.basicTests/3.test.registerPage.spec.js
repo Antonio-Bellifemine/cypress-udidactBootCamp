c102abbef6c9b57abf24d493c80ffc3335465827
abellifemine <abellifemine@bluegracegroup.com>
2020-10-11 20:18:05 -0400

    added string interpolation example

--- cypress/integration/tests/Module1.basicTests/3.test.registerPage.spec.js
+++ cypress/integration/tests/Module1.basicTests/3.test.registerPage.spec.js
@@ -24,9 +24,15 @@ describe('Registration Page Displays Properly', () => {
     it('display the username, email, password input fields', () => {
         cy.get('a:contains("Sign in")').click();
         cy.get('a:contains("Need an account?")').click();
-        cy.get('input:eq(0)').should('be.visible').type(`A${userLastName}`);
+        cy.get('input:eq(0)').should('be.visible').type("A" + userLastName);
         cy.get('input:eq(1)').should('be.visible').type(userEmail);
         cy.get('input:eq(2)').should('be.visible').type(passWord);
     });
+
+    // use string interpolation as an example of cleaner more readable code. (reads more like a sentance rather than 2 separate arguments)
+// cy.get('input:eq(0)').should('be.visible').type(`A${userLastName}`);
+// cy.get('input:eq(0)').should('be.visible').type("A" + userLastName);
+
+
 
 });
