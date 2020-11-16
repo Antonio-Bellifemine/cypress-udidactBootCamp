cd95b3a6045058023b242d7cf86a215e1941e512
abellifemine <abellifemine@bluegracegroup.com>
2020-11-15 19:14:54 -0500

    cleaned up module 1 to be more beginner friendly

--- cypress/integration/Module 1 - Some Basics/2.basic_Assertions.spec.js
+++ cypress/integration/Module 1 - Some Basics/2.basic_Assertions.spec.js
@@ -1,17 +1,24 @@
 before('visit registration page', () => {
-    cy.visit('register');
+    cy.visit('');
 })
 
-describe('Registration Page Displays Properly', () => {
+describe("We'll cover some basic assertions", () => {
 
-    it('verify conduit logo appears', () => {
+    it('verify nav bar on public landing page contains logo and expected buttons', () => {
         cy.get('a').contains('conduit').should('be.visible');
-        cy.get('h1').contains('Sign in').should('be.visible')
     });
 
-    it('verify sign in h1 and "have an account" link appear', () => {
-        cy.get('a').contains('conduit').should('be.visible');
-        cy.get('a').contains('Have an account?').should('be.visible')
+    // this test will pass and be misleading. be sure to specify the index number of the specific element
+    it('Confirm home button is visible in nav bar', () => {
+        cy.get('a.nav-link').eq(0).should('have.text', 'Home')
     });
-    
+
+    it('Confirm sign in button is visible in nav bar', () => {
+        cy.get('a.nav-link').eq(1).should('have.text', 'Sign in')
+    });
+
+    it('Confirm sign up button is visible in nav bar', () => {
+        cy.get('a.nav-link').eq(2).should('have.text', 'Sign up')
+    });
+
 });
---
+++ cypress/integration/Module 2/registration_Page.spec.js
@@ -0,0 +1,9 @@
+describe(() => {
+
+    it('verify h1 contains sign in and "have an account" link appear', () => {
+        cy.get('a').contains('conduit').should('be.visible');
+        cy.get('a').contains('Have an account?').should('be.visible')
+    });
+    
+})
+
