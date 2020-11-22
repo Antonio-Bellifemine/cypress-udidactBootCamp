b8b6f84ed03e2897fe424af0604196e13b27dd98
abellifemine <abellifemine@bluegracegroup.com>
2020-11-21 21:55:46 -0500

    make fix

--- cypress/integration/Module 1 - Some Basics/4.basic.Assertions.spec.js
+++ cypress/integration/Module 1 - Some Basics/4.basic.Assertions.spec.js
@@ -8,17 +8,16 @@ describe("We'll cover some basic assertions", () => {
         cy.get('a').contains('conduit').should('be.visible');
     });
 
-    // this test will pass and be misleading. be sure to specify the index number of the specific element
     it('Confirm home button is visible in nav bar', () => {
         cy.get('a.nav-link').eq(0).should('have.text', 'Home')
     });
 
-    it('Confirm sign in button is visible in nav bar', () => {
-        cy.get('a.nav-link').eq(1).should('have.text', 'Sign in')
-    });
-
     it('Confirm sign up button is visible in nav bar', () => {
         cy.get('a.nav-link').eq(2).should('have.text', 'Sign up')
+    });
+
+    it('Confirm sign in button is visible in nav bar', () => {
+        cy.get('a.nav-link').eq(1).should('contain', 'Sign in')
     });
 
 });
