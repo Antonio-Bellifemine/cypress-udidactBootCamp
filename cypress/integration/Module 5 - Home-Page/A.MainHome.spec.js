f0c0d1c8f2e9075ace4dec65f70133bba3dd301b
abellifemine <abellifemine@bluegracegroup.com>
2020-11-29 20:23:41 -0500

    clean up some junk

--- cypress/integration/Module 5 - Home-Page/A.MainHome.spec.js
+++ cypress/integration/Module 5 - Home-Page/A.MainHome.spec.js
@@ -2,11 +2,10 @@ before(() => {
     cy.visit('')
 });
 
-describe('', () => {
+describe('Main Home Page', () => {
 
-    it('verify h1 contains sign in and "have an account" link appear', () => {
-        cy.get('a').contains('conduit').should('be.visible');
-        cy.get('a').contains('Have an account?').should('be.visible')
+    it('', () => {
+
     });
     
 });
--- cypress/integration/Module 5 - Home-Page/B.Click Like BUtton.spec.js
+++ cypress/integration/Module 5 - Home-Page/B.Click Like BUtton.spec.js
@@ -2,22 +2,7 @@ before(() => {
     cy.visit('')
 });
 
-describe("Confirm clicking your feed link redirects to the sign in page while logged in", () => {
+describe("Clicking a like button while Logged in & on the Home Page", () => {
 
-    it("check to see if 'your feed' link is visible and click it", () => {
-        cy.get('a.nav-link').contains('Your Feed').should('be.visible').click();
-    });
-
-    it('confirm proper redirect to log in page by verifying URL', () => {
-        cy.url().should('equal', 'https://conduit-af252.firebaseapp.com/#/login')
-        cy.url().should('include', '#/login')
-    });
-
-    it('confirm expected elements display on login page', () => {
-        cy.get('h1').should('have.text', 'Sign in').and('be.visible');
-        // when using confirming a css color property, Cypress will return and match on rgb rather than hex so we'll need to convert hex to rgb
-        cy.get('a').contains('Need an account?').should('have.css', 'color').and('equal', 'rgb(92, 184, 92)')
-        cy.get('a').eq(4).should('have.attr', 'href').and('equal', '#/register')
-    });
 
 });
