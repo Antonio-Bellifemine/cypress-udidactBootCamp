247d61e76a3f239c3e649425db781f0c3828860d
abellifemine <abellifemine@bluegracegroup.com>
2020-12-02 21:37:30 -0500

    added login and logout commands/home test

--- cypress/integration/Module 5 - Home-Page/A.MainHome.spec.js
+++ cypress/integration/Module 5 - Home-Page/A.MainHome.spec.js
@@ -1,12 +1,16 @@
 before(() => {
-    cy.visit('')
+    cy.login('test9118@testing.com', 'UdidactCamp2020')
 });
 
 describe('Main Home Page', () => {
 
-    it('', () => {
-
+    it('log in', () => {
+        cy.get('a').contains('Your Feed').should('be.visible')
     });
     
 });
+
+after(() => {
+    cy.logout();
+})
 
