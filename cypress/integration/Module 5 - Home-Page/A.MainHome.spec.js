ac0b036a85dbf0266909eb9bf0237be3d01d8fbb
abellifemine <abellifemine@bluegracegroup.com>
2020-12-02 21:41:05 -0500

    comment on commands

--- cypress/integration/Module 5 - Home-Page/A.MainHome.spec.js
+++ cypress/integration/Module 5 - Home-Page/A.MainHome.spec.js
@@ -1,16 +1,15 @@
 before(() => {
-    cy.login('test9118@testing.com', 'UdidactCamp2020')
+    cy.login('test9118@testing.com', 'UdidactCamp2020');
 });
 
 describe('Main Home Page', () => {
 
     it('log in', () => {
-        cy.get('a').contains('Your Feed').should('be.visible')
+        cy.get('a').contains('Your Feed').should('be.visible');
     });
     
 });
 
 after(() => {
     cy.logout();
-})
-
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -2,8 +2,9 @@ import 'cypress-wait-until';
 const Chance = require('chance')
 const chance = new Chance()
 
+// Login
 Cypress.Commands.add('login', (userName, pswd) => {
-    let homeUrl = "https://conduit-af252.firebaseapp.com/#/"
+    let homeUrl = "https://conduit-af252.firebaseapp.com/#/";
     cy.visit('login');
     cy.get('input:eq(0)').type(userName, {delay: 100});
     cy.get('input:eq(1)').type(pswd, {delay: 100});
@@ -16,17 +17,17 @@ Cypress.Commands.add('login', (userName, pswd) => {
             errorMsg: 'This is a custom error message' // overrides the default error message
         });
 });
-
+// Logout
 Cypress.Commands.add('logout', () => {
-    let settingsUrl = 'https://conduit-af252.firebaseapp.com/#/profile/UdidactCamp2020'
-    cy.get('li.nav-item').contains(' UdidactCamp2020 ').click()
+    let settingsUrl = "https://conduit-af252.firebaseapp.com/#/profile/UdidactCamp2020";
+    cy.get('li.nav-item').contains(' UdidactCamp2020 ').click();
     cy.waitUntil(() =>
         cy.location('href').should('eql', settingsUrl),
         {
             timeout: 6000, // waits up to 2000 ms, default to 5000
             interval: 1000, // performs the check every 1000 ms, default to 200
             errorMsg: 'This is a custom error message' // overrides the default error message
-        })
+        });
     cy.get('a').contains(' Edit Profile Settings ').click();
     cy.get('button.btn-outline-danger').contains(' Or click here to logout. ').click();
 });
