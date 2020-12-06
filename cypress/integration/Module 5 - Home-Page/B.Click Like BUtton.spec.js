60925b062e5168d91b6f94630d3917da7561927f
abellifemine <abellifemine@bluegracegroup.com>
2020-12-06 11:32:11 -0500

    using cy.intercept & finishing Mod 5 Test B

--- cypress/integration/Module 5 - Home-Page/B.Click Like BUtton.spec.js
+++ cypress/integration/Module 5 - Home-Page/B.Click Like BUtton.spec.js
@@ -1,8 +1,37 @@
+let likeBtn = 'button';
+let globalFeed = 'div.article-preview';
+
 before(() => {
-    cy.visit('')
+    cy.login('test9118@testing.com', 'UdidactCamp2020');
 });
 
 describe("Clicking a like button while Logged in & on the Home Page", () => {
+    it('confirm like button increases by 1',() => {
+        let clickedLikeBtn = 'div.info'
+        // wait for the last global feed article to populate until we move forward
+        cy.waitUntil(() =>
+                cy.get('a').contains('Global Feed'),
+            {
 
+                timeout: 2000, // waits up to 2000 ms, default to 5000
+                interval: 500, // performs the check every 500 ms, default to 200
+                errorMsg: 'This is a custom error message' // overrides the default error message
 
+            }).click();
+            
+        /*    
+            we can use cy.intercept() to manage the behavior of HTTP requests at the network layer, which 
+            will allow us another way to wait for the expected behavior or results before moving on
+        */
+        cy.intercept('POST', '/favorite').as('articleFavoritedBtnClicked');
+        cy.get(likeBtn).eq(0).click();
+        cy.wait('@articleFavoritedBtnClicked');
+        cy.get(clickedLikeBtn).eq(1).should('have.text', "1");
+        cy.intercept('DELETE', '/favorite').as('articleFavoritedBtnDeleted');
+        cy.get(likeBtn).eq(0).click();
+        cy.wait('@articleFavoritedBtnDeleted');
+        cy.get('div.info').eq(1).should('have.text', "0");
+        
+        
+    });
 });
