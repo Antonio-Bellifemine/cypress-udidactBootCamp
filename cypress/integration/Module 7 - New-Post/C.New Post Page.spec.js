3444299747b695d4f168269f9c1aa2f67e60655e
abellifemine <abellifemine@bluegracegroup.com>
2020-12-07 21:01:56 -0500

    update mod 7 test A

--- cypress/integration/Module 7 - New-Post/C.New Post Page.spec.js
+++ cypress/integration/Module 7 - New-Post/C.New Post Page.spec.js
@@ -8,7 +8,8 @@ describe("New Post Page'", () => {
     it("Check to see if new post page has expected elements", () => {
         cy.get('a').eq(0);
         cy.get('a.author').eq(1).should('contain', ' UdidactCamp2020 ');
-        cy.get('a[class="btn btn-sm btn-outline-secondary"]').contains(' Edit Article ').should('be.visible')
+        cy.get('a[class="btn btn-sm btn-outline-secondary"]').eq(0).contains(' Edit Article ').should('be.visible')
+        cy.get('a[class="btn btn-sm btn-outline-secondary"]').eq(1).contains(' Edit Article ').should('be.visible')
         cy.get('button').contains(' Delete Article ').should('be.visible');
     });
 });
